d7e0d7863d0f348468f1d792fbdc78816aefdca6
singularitti <singularitti@outlook.com>
2023-03-01 18:33:38 -0500

    Use `@pymutable` in modules

--- src/parser.jl
+++ src/parser.jl
@@ -1,9 +1,5 @@
-mutable struct Parser
-    o::Py
-end
+@pymutable Parser o
 Parser() = Parser(f90nml.Parser())
-
-@pyinterface Parser
 
 read(p::Parser, nml_fname::AbstractString, nml_patch_in=nothing, patch_fname=nothing) =
     p.read(nml_fname, nml_patch_in, patch_fname)
