15ba67f056ae78cb21ff58a958e8bea8eef4ebaa
singularitti <singularitti@outlook.com>
2023-03-01 01:58:24 -0500

    Fix macro `@pyinterface`

--- src/init.jl
+++ src/init.jl
@@ -1,10 +1,5 @@
-__precompile__() # this module is safe to precompile
-
-using Conda: add_channel
-using PyCall: PyNULL, PyError, PyObject, PyAny, pyimport_conda, pycall
 using VersionParsing: vparse
-# Extending methods
-using PyCall: PyCall
+using PythonCall: PythonCall, Py, pynew, pyimport, pyhasattr, pyconvert, pycall, pycopy!
 
 export f90nml
 
@@ -22,27 +17,22 @@ function __init__()
 end
 
 macro pyinterface(T)
-    T = esc(T)
     return quote
-        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L54-L62
-        PyCall.PyObject(f::$T) = getfield(f, :o)
-        Base.convert(::Type{$T}, o::PyObject) = $T(o)
-        Base.:(==)(f::$T, g::$T) = PyObject(f) == PyObject(g)
-        Base.:(==)(f::$T, g::PyObject) = PyObject(f) == g
-        Base.:(==)(f::PyObject, g::$T) = f == PyObject(g)
-        Base.hash(f::$T) = hash(PyObject(f))
-        PyCall.pycall(f::$T, args...; kws...) = pycall(PyObject(f), args...; kws...)
-        (f::$T)(args...; kws...) = pycall(PyObject(f), PyAny, args...; kws...)
-        Base.Docs.doc(f::$T) = Base.Docs.doc(PyObject(f))
-        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
-        Base.getproperty(f::$T, s::Symbol) = getproperty(PyObject(f), s)
-        Base.getproperty(f::$T, s::AbstractString) = getproperty(PyObject(f), s)
-        Base.setproperty!(f::$T, s::Symbol, x) = setproperty!(PyObject(f), s, x)
-        Base.setproperty!(f::$T, s::AbstractString, x) = setproperty!(PyObject(f), s, x)
-        PyCall.hasproperty(f::$T, s::Symbol) = hasproperty(PyObject(f), s)
-        Base.propertynames(f::$T) = propertynames(PyObject(f))
-        Base.haskey(f::$T, x) = haskey(PyObject(f), x)
-        # Common methods
-        Base.close(f::$T) = PyObject(f).close()
+        # Code from https://github.com/stevengj/PythonPlot.jl/blob/d58f6c4/src/PythonPlot.jl#L65-L72
+        PythonCall.Py(f::$T) = getfield(f, :o)
+        PythonCall.pyconvert(::Type{$T}, o::Py) = $T(o)
+        Base.:(==)(f::$T, g::$T) = pyconvert(Bool, Py(f) == Py(g))
+        Base.isequal(f::$T, g::$T) = isequal(Py(f), Py(g))
+        Base.hash(f::$T, h::UInt) = hash(Py(f), h)
+        PythonCall.pycall(f::$T, args...; kws...) = pycall(Py(f), args...; kws...)
+        (f::$T)(args...; kws...) = pycall(Py(f), args...; kws...)
+        Base.Docs.doc(f::$T) = Base.Docs.Text(pyconvert(String, Py(f).__doc__))
+        # Code from https://github.com/stevengj/PythonPlot.jl/blob/d58f6c4/src/PythonPlot.jl#L75-L80
+        Base.getproperty(f::$T, s::Symbol) = getproperty(Py(f), s)
+        Base.getproperty(f::$T, s::AbstractString) = getproperty(Py(f), Symbol(s))
+        Base.setproperty!(f::$T, s::Symbol, x) = setproperty!(Py(f), s, x)
+        Base.setproperty!(f::$T, s::AbstractString, x) = setproperty!(Py(f), Symbol(s), x)
+        Base.hasproperty(f::$T, s::Symbol) = pyhasattr(Py(f), s)
+        Base.propertynames(f::$T) = propertynames(Py(f))
     end
-end # macro pyinterface
+end
